940402e3483866f8ea95e1ab4318522b91507005
kubakoltun <kubakoltun1232@gmail.com>
2023-04-05 12:20:30 +0200

    separate commit with the whole frontend file

---
+++ frontend/src/components/HomePage.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import RoomJoinPage from './RoomJoinPage';
+import CreateRoomPage from './CreateRoomPage';
+import Room from './Room';
+import { BrowserRouter, Route, Link, Redirect, Routes } from 'react-router-dom';
+// import { Route, Switch } from 'react-router';
+
+export default class HomePage extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <BrowserRouter>
+               <Routes>
+                    <Route path="/">
+                        <Route index element={<p>This is the home page</p>} />
+                    </Route>
+                    <Route path="/join" element={<RoomJoinPage />} />
+                    <Route path="/create" element={<CreateRoomPage />} />
+                    <Route path="/room/:roomCode" element={<Room />} />
+                </Routes>
+          </BrowserRouter>
+        );
+    }
+}
