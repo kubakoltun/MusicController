792ca5a9d28889bf9fbc6f03ae4120f58962f158
Jakub Kołtun <87318086+kubakoltun@users.noreply.github.com>
2023-04-21 14:56:25 +0200

    Update credentials.py

--- spotify/credentials.py
+++ spotify/credentials.py
@@ -1,3 +1,4 @@
 CLIENT_ID = "9317ff4d2fd54174a51535c0d4aa0a38"
 CLIENT_SECRET = "xxxxxxxxxxxxxxxxxxxxxx"
+# note for future updates - spotify api works only on premium accounts
 REDIRECT_URI = "http://127.0.0.1:8000/spotify/redirect"
